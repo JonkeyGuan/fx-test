59ef11b061838e9a12e6d138be8c15a9a56c4e8b
Jonkey Guan <jonkey.guan@gmail.com>
2021-06-08 16:17:56 +0800

    change jenkins to deduce ns

--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -9,9 +9,11 @@ source ${my_dir}/${resource}/env.conf
 token_files="${my_dir}/*.conf ${my_dir}/${resource}/*.conf"
 token_cmd=$(getTokenCmd ${token_files})
 
+namespace=$(oc project | awk -F "\"" '{print $2}')
+token_cmd=$(addTokenCmd "${token_cmd}" "namespace" "${namespace}")
+
 eval "${token_cmd} ${my_dir}/${resource}/jenkins-maven-slave-configmap.yaml" | oc apply -n ${namespace} -f -
 
 eval "${token_cmd} ${my_dir}/${resource}/jenkins-maven-slave-pvc.yaml" | oc -n ${namespace} apply -f -
 
 eval "${token_cmd} ${my_dir}/${resource}/jenkins-persistent.yaml" | oc -n ${namespace} apply -f -
-
